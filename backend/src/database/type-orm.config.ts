bb071fe24bd210618174cd642339a990996bc2a1
kwak9898 <kwak9898@gitbub.com>
2023-01-09 18:50:06 +0900

    logging ture -> false

--- backend/src/database/type-orm.config.ts
+++ backend/src/database/type-orm.config.ts
@@ -11,7 +11,7 @@ export const connectionOptions = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: [User],
 });
 
