2c9175357652ae1614f084f72282f5b8975fee34
kwak9898 <kwak9898@gitbub.com>
2023-01-09 18:47:17 +0900

    dotenv를 꼭 써주자

--- backend/src/database/type-orm.config.ts
+++ backend/src/database/type-orm.config.ts
@@ -1,13 +1,15 @@
 import { DataSource } from 'typeorm';
 import { User } from '../entity/user.entity';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export const connectionOptions = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: 'root',
-  password: 'root',
-  database: 'bave',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [User],
