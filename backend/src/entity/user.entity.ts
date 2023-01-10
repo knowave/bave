4768bbab51db7d9f2aea45201f2f5a9f1213892c
kwak9898 <kwak9898@gitbub.com>
2023-01-10 10:34:55 +0900

    user entity 수정

--- backend/src/entity/user.entity.ts
+++ backend/src/entity/user.entity.ts
@@ -1,14 +1,14 @@
 import { BeforeUpdate, Column, Entity, PrimaryColumn } from 'typeorm';
 import { BaseEntity } from '../base/base.entity';
 
-@Entity('oath_user', { schema: 'public' })
+@Entity('users', { schema: 'public' })
 export class User extends BaseEntity {
   @PrimaryColumn('varchar', {
     name: 'user_id',
     comment: '회원 아이디',
     unique: true,
   })
-  userId: string | undefined;
+  userId?: string;
 
   @Column('varchar', {
     name: 'password',
@@ -17,6 +17,14 @@ export class User extends BaseEntity {
     select: false,
   })
   password?: string;
+
+  @Column('varchar', {
+    name: 'username',
+    comment: '회원 사용 이름',
+    nullable: true,
+  })
+  username?: string;
+
   @Column('varchar', {
     name: 'jwt_token',
     comment: 'jwt refresh token',
@@ -24,32 +32,6 @@ export class User extends BaseEntity {
     select: false,
   })
   jwtToken?: string;
-  @Column('varchar', {
-    name: 'username',
-    comment: '관리자 이름',
-    nullable: true,
-  })
-  username?: string;
-  @Column('varchar', {
-    name: 'team',
-    comment: '소속',
-    nullable: true,
-  })
-  team?: string;
-  @Column('varchar', {
-    name: 'role_name',
-    comment: '역할',
-    nullable: false,
-    default: '선택',
-  })
-  roleName?: string;
-
-  @Column('timestamp', {
-    name: 'last_access_at',
-    comment: '최근 접속일',
-    nullable: true,
-  })
-  lastAccessAt?: Date;
 
   @BeforeUpdate()
   async updateDate(): Promise<void> {
