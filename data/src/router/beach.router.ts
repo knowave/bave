2da9e978ae408c27af86741aab957eceae7e3de5
kwak9898 <kwak9898@gitbub.com>
2023-01-11 16:51:14 +0900

    Open API Data Insert In DB

--- data/src/router/beach.router.ts
+++ data/src/router/beach.router.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import request from 'request';
 import dotenv from 'dotenv';
+import convert from 'xml-js';
 import { dataSourceConfig } from '../database/data-source.config';
 import { Beach } from '../entity/beach.entity';
 
@@ -12,21 +13,24 @@ router.get('/', async (req: Request, res: Response) => {
   const key = process.env.SERVICE_KEY;
   try {
     for(let num = 1; num <= 10; num++) {
-      const requestUrl = 'https://www.meis.go.kr/service/OceansBeachInfoService/getOceansBeachInfo?serviceKey=' + key + encodeURI('&pageNo=1&numOfRows=10&SIDO_NM=강원&resultType=json&SG_APIM=2ug8Dm9qNBfD32JLZGPN64f3EoTlkpD8kSOHWfXpyrY');
+      const requestUrl = 'https://www.meis.go.kr/service/OceansBeachInfoService/getOceansBeachInfo?serviceKey=' + key + encodeURI('&pageNo=1&numOfRows=10&SIDO_NM=전남&resultType=xml&SG_APIM=2ug8Dm9qNBfD32JLZGPN64f3EoTlkpD8kSOHWfXpyrY');
       request(requestUrl, async (error, res, body) => {
         if (error) {
           console.error(`Request Error: ${error}`);
         } else {
-          const info = res.body;
-          console.log('INFO: ', res.body.item);
+          const result = body;
+          const xmlToJson = convert.xml2json(result, { compact: true, spaces: 4 });
+          const info = JSON.parse(xmlToJson).response.body.items.item;
+          // console.log('XML TO JSON : ', JSON.parse(xmlToJson).response.body.items.item[1].sidoNm._text);
+
           for (const i in info) {
-            console.log('RESPONSE BODY : ', info[i]);
+            // console.log('RESPONSE BODY : ', info[i]);
            const beachData = await beachRepository.create({
-             sidoName: info[i]['sido_nm'],
-             gugun_name: info[i]['gugun_nm'],
-             beachName: info[i]['sta_nm'],
-             latitude: info[i]['lat'],
-             longitude: info[i]['lon']
+             sidoName: info[i].sidoNm._text,
+             gugun_name: info[i].gugunNm._text,
+             beachName: info[i].staNm._text,
+             latitude: info[i].lat._text,
+             longitude: info[i].lon._text,
            });
            await beachRepository.save(beachData);
           }
