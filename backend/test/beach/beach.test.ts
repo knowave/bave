91f05a72fa6f94266fc6883af9c63a48f77f01ec
kwak9898 <kwak9898@gitbub.com>
2023-01-16 15:56:50 +0900

    beach test code

--- backend/test/beach/beach.test.ts
+++ backend/test/beach/beach.test.ts
@@ -3,17 +3,104 @@ import { STATUS_CODE } from '../../src/exception/status-code';
 import assert from 'assert';
 
 describe('해수욕장 API 테스트', function () {
-  // let beach: any;
+  let beach: any;
+  let feed: any;
+  let reply: any;
+  let like: any;
+
+  let beachId: number;
+  let feedId: number;
+  let replyId: number;
+  let likeId: number;
+
   let client: Client;
 
   describe('성공', function () {
     before(async () => {
       client = await Client.getInstance();
-      // beach = await client.getAllBeach();
+      beach = await client.getAllBeach();
+      feed = await client.createFeedByBeachId();
+      reply = await client.createReplyByBeachId();
+      like = await client.insertLikeByFeed();
+
+      beachId = beach.body.id;
+      feedId = feed.body.id;
+      replyId = reply.body.id;
+      likeId = like.body.id;
     });
+
     it('해수욕장 전체 조회', async () => {
       const response = await client.getAllBeach();
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
 
+    it('특정 해수욕장 조회', async () => {
+      const response = await client.getBeachById(beachId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장에 피드 작성', async () => {
+      const response = await client.createFeedByBeachId(beachId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.CREATED);
+    });
+
+    it('특정 해수욕장 피드 전체 조회', async () => {
+      const response = await client.getAllFeedByBeachId(beachId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 상세 조회', async () => {
+      const response = await client.getFeedByBeachId(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 수정', async () => {
+      const response = await client.updateFeedById(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 삭제', async () => {
+      const response = await client.deleteFeedById(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 댓글 작성', async () => {
+      const response = await client.createReplyByBeachId(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.CREATED);
+    });
+
+    it('특정 해수욕장 피드 댓글 전체 조회', async () => {
+      const response = await client.getAllReplyByBeachId(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 댓글 수정', async () => {
+      const response = await client.updateReplyByFeed(beachId, feedId, replyId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 댓글 삭제', async () => {
+      const response = await client.deleteReplyByFeed(beachId, feedId, replyId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 좋아요', async () => {
+      const response = await client.insertLikeByFeed(beachId, feedId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.CREATED);
+    });
+
+    it('특정 해수욕장 피드 좋아요 취소', async () => {
+      const response = await client.cancelLikeByFeed(beachId, feedId, likeId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
+    });
+
+    it('특정 해수욕장 피드 댓글 좋아요', async () => {
+      const response = await client.insertReplyLikeByFeed(beachId, feedId, replyId);
+      assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.CREATED);
+    });
+
+    it('특정 해수욕장 피드 댓글 좋아요 취소', async () => {
+      const response = await client.cancelReplyLikeByFeed(beachId, feedId, replyId, likeId);
       assert.equal(response.body.statusCode, STATUS_CODE.SUCCESS.OK);
     });
   });
--- backend/test/client.ts
+++ backend/test/client.ts
@@ -18,6 +18,9 @@ export default class Client {
     return client;
   }
 
+  /**
+   * 회원가입
+   */
   async signUp(userId = faker.internet.userName(), data?: string) {
     const signUpUrl = `${host}/sign-up`;
 
@@ -34,10 +37,153 @@ export default class Client {
     return response;
   }
 
+  /**
+   * 해수욕장 전체 조회
+   */
   async getAllBeach() {
     const beachListUrl = `${host}/beach`;
 
     const response = await request(this.app).get(`${beachListUrl}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 조회
+   */
+  async getBeachById(beachId?: number) {
+    const beachUrl = `${host}/beach/${beachId}`;
+    const response = await request(this.app).get(`${beachUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장에 피드 작성
+   */
+  async createFeedByBeachId(beachId?: number) {
+    const beachUrl = `${host}/feed/${beachId}`;
+    const response = await request(this.app).post(`${beachUrl}`).send({ content: `test-feed` }).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 전체 조회
+   */
+  async getAllFeedByBeachId(beachId?: number) {
+    const beachUrl = `${host}/feed/${beachId}`;
+    const response = await request(this.app).get(`${beachUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 상세 조회
+   */
+  async getFeedByBeachId(beachId?: number, feedId?: number) {
+    const beachUrl = `${host}/feed/${beachId}/${feedId}`;
+    const response = await request(this.app).get(`${beachUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 수정
+   */
+  async updateFeedById(beachId?: number, feedId?: number) {
+    const feedUrl = `${host}/feed/${beachId}/${feedId}`;
+    const response = await request(this.app).patch(`${feedUrl}`).send({ content: 'update-feed' }).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 삭제
+   */
+  async deleteFeedById(beachId?: number, feedId?: number) {
+    const feedUrl = `${host}/feed/${beachId}/${feedId}`;
+    const response = await request(this.app).delete(`${feedUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 작성
+   */
+  async createReplyByBeachId(beachId?: number, feedId?: number) {
+    const replyUrl = `${host}/reply/${beachId}/${feedId}`;
+    const response = await request(this.app).post(`${replyUrl}`).send({ contents: 'test-reply' }).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 전체 조회
+   */
+  async getAllReplyByBeachId(beachId?: number, feedId?: number) {
+    const replyUrl = `${host}/reply/${beachId}/${feedId}`;
+    const response = await request(this.app).get(`${replyUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 수정
+   */
+  async updateReplyByFeed(beachId?: number, feedId?: number, replyId?: number) {
+    const replyUrl = `${host}/reply/${beachId}/${feedId}/${replyId}`;
+    const response = await request(this.app).patch(`${replyUrl}`).send({ contents: 'update-reply' }).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 삭제
+   */
+  async deleteReplyByFeed(beachId?: number, feedId?: number, replyId?: number) {
+    const replyUrl = `${host}/reply/${beachId}/${feedId}/${replyId}`;
+    const response = await request(this.app).delete(`${replyUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 좋아요
+   */
+  async insertLikeByFeed(beachId?: number, feedId?: number) {
+    const likeUrl = `${host}/like/${beachId}/${feedId}`;
+    const response = await request(this.app).post(`${likeUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 좋아요 취소
+   */
+  async cancelLikeByFeed(beachId?: number, feedId?: number, likeId?: number) {
+    const likeUrl = `${host}/like/${beachId}/${feedId}/${likeId}`;
+    const response = await request(this.app).post(`${likeUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 좋아요
+   */
+  async insertReplyLikeByFeed(beachId?: number, feedId?: number, replyId?: number) {
+    const replyLikeUrl = `${host}/like/${beachId}/${feedId}/${replyId}`;
+    const response = await request(this.app).post(`${replyLikeUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
+
+    return response;
+  }
+
+  /**
+   * 특정 해수욕장 피드 댓글 좋아요 취소
+   */
+  async cancelReplyLikeByFeed(beachId?: number, feedId?: number, replyId?: number, likeId?: number) {
+    const replyLikeUrl = `${host}/like/${beachId}.${feedId}/${replyId}/${likeId}`;
+    const response = await request(this.app).post(`${replyLikeUrl}`).set('Authorization', `Bearer${this.jwt.accessToken}`);
 
     return response;
   }
