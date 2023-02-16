ab185c885d2ae666a6c2b850942fff8d6ea58f16
kwak9898 <kwak9898@gitbub.com>
2023-02-16 14:59:58 +0900

    multer 수정

--- backend/src/domains/feed/middleware/upload.middleware.ts
+++ backend/src/domains/feed/middleware/upload.middleware.ts
@@ -4,7 +4,7 @@ import multerS3 from 'multer-s3';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const s3 = new S3Client({
+export const s3 = new S3Client({
   region: process.env.AWS_S3_REGION,
   credentials: {
     accessKeyId: String(process.env.AWS_SE_ACCESS_KEY_AWS),
@@ -12,7 +12,15 @@ const s3 = new S3Client({
   },
 });
 
+const imageFilter = (req: any, file: any, cb: any) => {
+  if (!file.originalname.match(/\.(JPG|jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+
 export const upload = multer({
+  fileFilter: imageFilter,
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_S3_BUCKET || '',
@@ -22,7 +30,7 @@ export const upload = multer({
       cb(null, { fieldName: file.fieldname });
     },
     key: (req, file, cb) => {
-      cb(null, Date.now().toString());
+      cb(null, `${Date.now().toString()}.jpg`);
     },
   }),
 });
