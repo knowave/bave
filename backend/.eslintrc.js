b65e364f6cb7ca47b0f969bbabc433546d47c6c9
kwak9898 <kwak9898@gitbub.com>
2023-02-21 10:36:52 +0900

    eslint 수정

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: "tsconfig.json",
+        createDefaultProgram: true,
         tsconfigRootDir: __dirname,
         sourceType: "module",
     },
