6e4f4863be44baf9639e1bf0151f23a5984567fe
kwak9898 <ktm9837@naver.com>
2023-05-03 16:21:05 +0900

    sync true -> false

--- data/src/database/data-source.config.ts
+++ data/src/database/data-source.config.ts
@@ -1,20 +1,20 @@
-import { DataSource } from 'typeorm';
-import dotenv from 'dotenv';
-import { Beach } from '../entity/beach.entity';
-import { Like } from '../entity/like.entity';
-import { Reply } from '../entity/reply.entity';
-import { Feed } from '../entity/feed.entity';
-import { User } from '../entity/user.entity';
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+import { Beach } from "../entity/beach.entity";
+import { Like } from "../entity/like.entity";
+import { Reply } from "../entity/reply.entity";
+import { Feed } from "../entity/feed.entity";
+import { User } from "../entity/user.entity";
 
 dotenv.config();
 
 export const dataSourceConfig = new DataSource({
-  type: 'mysql',
+  type: "mysql",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [User, Beach, Feed, Reply, Like],
-  synchronize: true,
+  synchronize: false,
 });
